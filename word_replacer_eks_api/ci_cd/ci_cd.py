bdce75d16ae42fa294d14e7334ef30e52549643c
SanthoshParapatiTR <santoshparapati.reddy@thomsonreuters.com>
2022-10-20 17:08:41 +0530

    update ci cd buildspec

--- word_replacer_eks_api/ci_cd/ci_cd.py
+++ word_replacer_eks_api/ci_cd/ci_cd.py
@@ -10,43 +10,43 @@ class CICDStack(Stack):
 
 
         source_output = codepipeline.Artifact(artifact_name='app_source')
-        build_spec = codebuild.BuildSpec.from_object({                 
-                            "version": '0.2',
-                            "phases": {
-                            "pre_build": {
-                                "commands": [
-                                'ls'
-                                'env',
-                                'export TAG=${CODEBUILD_RESOLVED_SOURCE_VERSION}',
-                                'export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output=text)', 
-                                'echo Logging in to Amazon ECR',
-                                'aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 349115202997.dkr.ecr.us-east-1.amazonaws.com',
-                                ],
-                            },
-
-                            # aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 349115202997.dkr.ecr.us-east-1.amazonaws.com
-
-                            # sudo docker build -t word_replacer_api .
-
-                            # sudo docker tag word_replacer_api:latest 349115202997.dkr.ecr.us-east-1.amazonaws.com/word_replacer_api:latest
-
-                            # sudo docker push 349115202997.dkr.ecr.us-east-1.amazonaws.com/word_replacer_api:latest
-                            "build": {
-                                "commands": [
-                                'cd ',
-                                'ocker build -t word_replacer_api:$TAG .',
-                                'docker push 349115202997.dkr.ecr.us-east-1.amazonaws.com/word_replacer_api:$TAG',
-                                ],
-                            },
-                            "post_build": {
-                                "commands": [
-                                'kubectl get no',
-                                'kubectl set image deployment word_replacer_api flask=word_replacer_api:$TAG',
-                                ],
-                            },
-                            },
-                        })
-        
+        build_spec = codebuild.BuildSpec.from_object({
+                    "version": "0.2",
+                    "phases": {
+                        "pre_build": {
+                        "commands": [ 
+                            "export TAG=${CODEBUILD_BUILD_NUMBER}",
+                            "export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output=text)",
+                            "apt-get update && apt-get -y install jq python3-pip python3-dev && pip3 install --upgrade awscli",
+                            "curl -sS -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl",
+                            "chmod +x ./kubectl " , 
+                            "echo Logging in to Amazon ECR",
+                            "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 349115202997.dkr.ecr.us-east-1.amazonaws.com"
+                        ]
+                        },
+                        "build": {
+                        "commands": [ 
+                            "cd word_replacer_eks_api/word_replacer_app/",
+                            "docker build -t word_replacer_api:$TAG .",
+                            "docker tag word_replacer_api:$TAG 349115202997.dkr.ecr.us-east-1.amazonaws.com/word_replacer_api:$TAG",
+                            "docker push 349115202997.dkr.ecr.us-east-1.amazonaws.com/word_replacer_api:$TAG"
+                        ]
+                        
+                        },
+                        "post_build": {
+                        "commands": [
+                            "aws sts assume-role --role-arn arn:aws:iam::349115202997:role/VPCEKS-EKSVPCEKSDED15206-ClusterAdminRole047D4FCA-XCOTSMISXLJX --role-session-name codebuild-kubectl --duration-seconds 900",
+                            "aws eks update-kubeconfig --name EKS_Cluster-VPCEKS --region us-east-1 --role-arn arn:aws:iam::349115202997:role/VPCEKS-EKSVPCEKSDED15206-ClusterAdminRole047D4FCA-XCOTSMISXLJX",
+                            "export KUBECONFIG=/root/.kube/config",
+                            "cd word_replacer_eks_api/word_replacer_app/",
+                            "kubectl apply -f deployment.yml ",
+                            "kubectl set image deployment word-replacer-api word-replacer-api=word_replacer_api:$TAG"
+                        ]
+                        }
+                    }
+                }
+             )
+                            
   
         build_ecr_image = codebuild.PipelineProject(
                         self,
